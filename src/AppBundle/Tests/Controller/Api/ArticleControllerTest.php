fe799094cd5aefd8b004bc2e89fbf0c62a26df9b
Koray <koraykupe@gmail.com>
2017-05-18 14:54:19 +0300

    Test Get Article Endpoint

--- src/AppBundle/Tests/Controller/Api/ArticleControllerTest.php
+++ src/AppBundle/Tests/Controller/Api/ArticleControllerTest.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Tests\Controller\Api;
 
+use AppBundle\Entity\Article;
+use Doctrine\ORM\EntityManager;
 use Faker\Factory;
+use GuzzleHttp\Client;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class ArticleControllerTest extends ApiTestCase
 {
-    public function testPOST()
+    public function testPOSTArticle()
     {
         // Generate Fake Data
         $faker = Factory::create();
@@ -30,5 +34,49 @@ class ArticleControllerTest extends ApiTestCase
         $this->assertSame($content, $responseData['content']);
         $this->assertArrayHasKey('title', $responseData);
         $this->assertArrayHasKey('content', $responseData);
+    }
+
+    public function testGETArticle()
+    {
+        $article = $this->createArticle();
+
+        $response = $this->client->get('/api/articles/'.$article->getId());
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = \GuzzleHttp\json_decode($response->getBody(), true);
+        
+        $this->assertEquals(array(
+            'title',
+            'content'
+        ), array_keys($data));
+    }
+
+    protected function createArticle()
+    {
+        // Generate Fake Data
+        $faker = Factory::create();
+        $title = $faker->sentence(6);
+        $content = $faker->text();
+
+        $data = array(
+            'title' =>  $title,
+            'content' => $content
+        );
+
+        $accessor = PropertyAccess::createPropertyAccessor();
+        $article = new Article();
+        foreach ($data as $key => $value) {
+            $accessor->setValue($article, $key, $value);
+        }
+        $this->getEntityManager()->persist($article);
+        $this->getEntityManager()->flush();
+        return $article;
+    }
+
+    /**
+     * @return EntityManager
+     */
+    protected function getEntityManager() :EntityManager
+    {
+        return $this->getService('doctrine.orm.entity_manager');
     }
 }
